c797a3db071d419279b0b07f37e91f8a4e282051
SomeSuperCoder <ithelper9@gmail.com>
2026-01-30 17:54:00 +0300

    Basic Hello

---
+++ services/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/SomeSuperCoder/HumaExampleProject/handlers"
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humagin"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	api := humagin.New(r, huma.DefaultConfig(
+		"Huma + Gin API",
+		"1.0.0",
+	))
+
+	huma.Get(api, "/hello", handlers.Hello)
+
+	r.Run(":8888")
+}
