af94996af5daf72550fa7dfcc47baf4c899ebe61
SomeSuperCoder <ithelper9@gmail.com>
2026-01-30 22:28:51 +0300

    .

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
-all: 
+all: postgres migrate
+
+serve:
+	go run services/api/main.go
 
 postgres:
 	docker-compose -f db/docker-compose.yaml up -d
 
 postgres-down:
 	docker-compose -f db/docker-compose.yaml down
+
+migrate:
+	goose up
+	sqlc generate
--- services/api/main.go
+++ services/api/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/SomeSuperCoder/HumaExampleProject/handlers"
+	"github.com/SomeSuperCoder/HumaExampleProject/internal"
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humagin"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	ctx := context.Background()
+	config := internal.LoadAppConfig()
+	pool, repo := internal.DatabaseConnect(ctx, config)
+	defer pool.Close()
+
 	r := gin.Default()
 
 	api := humagin.New(r, huma.DefaultConfig(
@@ -17,5 +26,9 @@ func main() {
 
 	huma.Get(api, "/hello", handlers.Hello)
 
-	r.Run(":8888")
+	orderHandler := handlers.OrderHandler{Repo: repo}
+	huma.Get(api, "/orders", orderHandler.GetAll)
+	huma.Post(api, "/orders", orderHandler.Post)
+
+	r.Run(fmt.Sprintf(":%s", config.Port))
 }
