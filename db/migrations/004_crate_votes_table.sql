8136f047a3a20f868abd2025f2e629de417d3ce0
SomeSuperCoder <ithelper9@gmail.com>
2026-02-05 16:21:39 +0300

    Cascade delete votes

--- db/migrations/004_crate_votes_table.sql
+++ db/migrations/004_crate_votes_table.sql
@@ -4,7 +4,7 @@ CREATE TYPE vote_type AS ENUM ('upvote', 'downvote');
 CREATE TABLE votes (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 
-  review UUID NOT NULL REFERENCES reviews(id),
+  review UUID NOT NULL REFERENCES reviews(id) ON DELETE CASCADE,
   voter UUID REFERENCES users(id) ON DELETE SET NULL,
   UNIQUE (review, voter),
   
